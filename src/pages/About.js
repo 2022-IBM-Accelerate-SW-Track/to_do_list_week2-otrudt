432b66ee7a8e33e322c5614c7f8b9324f255f5c3
rhit-trudtoj <78326617+rhit-trudtoj@users.noreply.github.com>
2022-06-15 21:57:03 -0400

    lets go OwenT

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,39 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import owenPic from '../images/owenPic.jpg'; 
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
+        <div className='split left'>
+          <div className='centered'>
+            <img className='profile_image' src={owenPic} alt="this is awkward..."></img>
           </div>
         </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+
+        <div className='split right'>
+          <div className='centered'>
+            <div className='name_title'>Hi my name is Owen Trudt</div>
+            <div className='brief_description'>
+              I am a computer science and biology student at Rose-Hulman Insitute of technology. 
+              I am also a collegiate athlete, I play men's soccer for Rose-Hulman. 
+              In my free time when I am not drowning in school work, I love to play soccer, read books, thrift, play video games, 
+              and watch movies/anime. 
+              I also like to hangout with my friends and family. 
+              I am a big supporter of Tottenham Hotspur (sadly). 
+              My favorite book is Spring Snow by Yukio Mishima. 
+              My favorite movie is Bladerunner 2049. 
+              My favorite anime is Neon Genesis Evangelion. 
+              My favorite anime movie is Weathering With You. 
             </div>
           </div>
         </div>
+
       </div>
-    );
+
+
+      
+    )
   }
 }
